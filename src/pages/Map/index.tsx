4b180cfe4355c02f7c1a14f206618e47feabaf78
shengbenqiang <3122971186@qq.com>
2022-04-15 16:53:53 +0800

    首页列表，头部导航和栅格小组页面展示及接口对接完成，百度地图 api 配合 typeScript 接入使用，语法通过，但未成功渲染地图组件；

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import "./Map.css";
 
-declare const BMap: any;
-
 const Map = () => {
 
     const createMap = () => {
-        const map = new BMap.Map("container")
-        const point = new BMap.Point(116.404, 39.915)
-        map.centerAndZoom(point, 15)
+        // const map = new BMap.Map("container")
+        // const point = new BMap.Point(116.404, 39.915)
+        console.log(document.getElementById("container"))
+        // map.centerAndZoom(point, 15)
     }
 
     useEffect(() => {
